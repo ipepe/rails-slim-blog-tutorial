8a45a91bcf4422b3056748f2d7d50db4a8010415
Patryk Ptasinski <gihub@ipepe.pl>
2015-07-13 06:07:43 -0700

    slug for articles created

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,9 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
             length: { minimum: 5 }
+
+  # that is simple way how to slug, better way with slug history and stuff is friendly-id
+  def to_param
+    "#{id} #{title}".parameterize
+  end
 end
