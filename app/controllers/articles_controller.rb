0a3d9b1290687bdcebe392b16e5511ae6dbfdd65
Patryk Ptasinski <gihub@ipepe.pl>
2015-07-13 04:12:08 -0700

    big step 5 finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,28 @@
 class ArticlesController < ApplicationController
-
   def index
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   def update
@@ -18,22 +35,11 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def show
+  def destroy
     @article = Article.find(params[:id])
-  end
+    @article.destroy
 
-  def new
-    @article = Article.new
-  end
-
-  def create
-    @article = Article.new(article_params)
-
-    if @article.save
-      redirect_to @article
-    else
-      render 'new'
-    end
+    redirect_to articles_path
   end
 
   private
